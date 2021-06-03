2bc2ce1ba64c2642fbe82ba87b7efc92b53b318f
Sam Lumley <samlumley@Sams-borrowed-MacBook-Pro.local>
2021-06-03 14:54:53 -0400

    added news accordian

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleTable() {
+    var x = document.getElementById("hidden-table");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
